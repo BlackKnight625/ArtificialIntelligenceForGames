a7e32177a615d183502084448737edbaccb48097
JoaoPMarto <joaopomarto@tecnico.ulisboa.pt>
2022-10-04 22:42:54 +0100

    added decision making

---
+++ IAJ - Decision Making/Assets/Scripts/Game/Monsters/Orc.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using Assets.Scripts.IAJ.Unity.Utils;
+using UnityEngine.AI;
+using Assets.Scripts.IAJ.Unity.DecisionMaking.BehaviorTree;
+using Assets.Scripts.IAJ.Unity.DecisionMaking.BehaviorTree.BehaviourTrees;
+using Assets.Scripts.IAJ.Unity.Formations;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.Game.NPCs
+{
+
+    public class Orc : Monster
+    {
+        public Orc()
+        {
+            this.enemyStats.Type = "Orc";
+            this.enemyStats.XPvalue = 10;
+            this.enemyStats.AC = 14;
+            this.baseStats.HP = 15;
+            this.DmgRoll = () => RandomHelper.RollD10() + 2;
+            this.enemyStats.SimpleDamage = 5;
+            this.enemyStats.AwakeDistance = 10;
+            this.enemyStats.WeaponRange = 3;
+        }
+
+
+        public override void InitializeBehaviourTree()
+        {
+            this.BehaviourTree = new BasicTree(this, this.Target);
+        }
+    }
+}
---
+++ IAJ - Decision Making/Assets/Scripts/IAJ.Unity/Formations/FormationManager.cs
@@ -0,0 +1,94 @@
+﻿using UnityEditor;
+using UnityEngine;
+using Assets.Scripts.Game;
+using Assets.Scripts.Game.NPCs;
+using System.Collections.Generic;
+using UnityEngine.AI;
+
+namespace Assets.Scripts.IAJ.Unity.Formations
+{
+    public class FormationManager
+    {
+        public Dictionary<Monster, int> SlotAssignment = new Dictionary<Monster, int>();
+
+        private FormationPattern pattern;
+
+        // # A Static (i.e., position and orientation) representing the
+        // # drift offset for the currently filled slots.
+
+        public Vector3 AnchorPosition;
+
+        public Vector3 Orientation;
+
+        
+        public FormationManager(List<Monster> NPCs, FormationPattern pattern, Vector3 position, Vector3 orientation )
+        {
+            this.pattern = pattern;
+            this.AnchorPosition = position;
+
+            int i = 0;
+            foreach (Monster npc in NPCs)
+            {
+                SlotAssignment[npc] = i;
+                i++;
+            }
+            // the pattern may define specific rules for the orientation...
+            this.Orientation = pattern.GetOrientation( this, orientation );
+        }
+
+        public void UpdateSlotAssignements()
+        {
+            int i = 0; 
+            foreach(var npc in  SlotAssignment.Keys)
+            {
+                SlotAssignment[npc] = i;
+                i++;
+            }
+        }
+
+        public bool AddCharacter(Monster character)
+        {
+            var occupiedSlots = this.SlotAssignment.Count;
+            if (this.pattern.SupportSlot(occupiedSlots + 1))
+            {
+                SlotAssignment.Add(character, occupiedSlots);
+                this.UpdateSlotAssignements();
+                return true;
+            }
+            else return false;
+        }
+
+        public void RemoveCharacter(Monster character)
+        {
+            var slot = SlotAssignment[character];
+            SlotAssignment.Remove(character);
+            UpdateSlots();
+        }
+
+        public void UpdateSlots()
+        {
+            var anchor = AnchorPosition;
+
+            var orientationMatrix = Orientation;
+
+
+            foreach (var npc in SlotAssignment.Keys)
+            {
+                int slotNumber = SlotAssignment[npc];
+                var slot = pattern.GetSlotLocation(this, slotNumber);
+
+                var locationPosition = anchor + orientationMatrix * slotNumber;
+                var locationOrientation = orientationMatrix + slot;
+
+                // and add drift componenet.
+
+                locationPosition -= Vector3.one * 0.1f;
+                //locationOrientation -= 0.1f;
+
+                
+                npc.MoveTo(locationPosition);
+                npc.GetComponent<NavMeshAgent>().updateRotation = true;
+            }
+        }
+    }
+}
