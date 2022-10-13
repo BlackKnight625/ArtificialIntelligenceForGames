9c74e5c59efaeda8491034cc8162cadcc27733d3
BlackKnight625 <43484611+BlackKnight625@users.noreply.github.com>
2022-10-13 11:35:15 +0100

    Added project

---
+++ IAJ - Decision Making/Assets/Scripts/IAJ.Unity/DecisionMaking/BehaviorTree/BehaviourTrees/OrcTree.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Assets.Scripts.Game.NPCs;
+using Assets.Scripts.IAJ.Unity.DecisionMaking.BehaviorTree.EnemyTasks;
+using UnityEngine;
+
+namespace Assets.Scripts.IAJ.Unity.DecisionMaking.BehaviorTree.BehaviourTrees {
+    class OrcTree : Sequence {
+        public OrcTree(Monster character, GameObject target)
+        {
+            // To create a new tree you need to create each branck which is done using the constructors of different tasks
+            // Additionally it is possible to create more complex behaviour by combining different tasks and composite tasks...
+            this.children = new List<Task>()
+            {
+                new IsCharacterNearTarget(character, target, character.enemyStats.AwakeDistance),
+                new MoveTo(character, target, character.enemyStats.WeaponRange),
+                new LightAttack(character)
+            };
+        }
+    }
+}
